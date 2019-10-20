921a02744d938acc24b6767414d04a0543d218c0
Matko <matkobusljeta@gmail.com>
2019-10-20 22:58:08 +0200

    Finished first version of the program

---
+++ GetRandomMovie.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import csv
+import random
+
+movies = []
+with open('WATCHLIST.csv', encoding='latin_1') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    for row in csv_reader:
+        if row[5] != 'Title':  # don't add first row
+
+            # create dictionary for each movie
+            movie = {'title': row[5], 'link': row[6]}
+            movies.append(movie)
+
+selected_movie = random.choice(movies)
+print('Randomly selected movie: ' + selected_movie['title'] + '\nLink: '
+      + selected_movie['link'])
